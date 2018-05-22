b5781e18813ea84f111b4ec3b71af4f9aed8efac
William Kimball <wkkimball043@gmail.com>
2018-05-22 12:06:54 -0500

    continued working on main.js in order to ensure component based nature of the extra extra work

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 let articleContent = ""
 
 let basicInfo = document.createElement("section");
-basicInfo.className = "candidate__info"
-basicInfo.innerHTML = '<img src="scottj.jpeg" alt="Image of Scott DesJarlais"> <h1>Scott DesJarlais</h1> <h2>Representative for Tennessee&#39;s 4th congressional district</h2><h3>Republican</h3>'
-console.log(basicInfo)
+basicInfo.id = "candidate__info"
+
+let candidate__info__image = document.createElement("img");
+candidate__info__image.setAttribute("src", "scottj.jpeg");
+candidate__info__image.setAttribute("alt", "Image of Scott DesJarlais");
+basicInfo.appendChild(candidate__info__image);
+
+let candidate__info__name = document.createElement("h1");
+candidate__info__name.textContent = "Scott DesJarlais";
+basicInfo.appendChild(candidate__info__name);
+
+let candidate__info__rep = document.createElement("h2");
+candidate__info__rep.textContent = "Representative for Tennessee's 4th congressional district"
+basicInfo.appendChild(candidate__info__rep);
+
+let candidate__info__affiliation = document.createElement("h3");
+candidate__info__affiliation.textContent = "Republican"
+basicInfo.appendChild(candidate__info__affiliation);
+
 document.querySelector("article").appendChild(basicInfo)
 
 
 let committees = document.createElement("section");
-committees.className = "candidate__committiees";
+committees.id = "candidate__committiees";
 committees.innerHTML = '<h2>House Committee on Agriculture</h2><h2>House Committee on Armed Services</h2><h2>House Committee on Oversight and Government Reform</h2>'
 articleContent.innerHTML += "committees"
 document.querySelector("article").appendChild(committees)
 
 let links = document.createElement("section");
-links.className = "candidate__links";
+links.id = "candidate__links";
 links.innerHTML = '<a href="https://www.govtrack.us/congress/bills/browse?sponsor=412477">Link to Sponsored Legislation</a>'
 console.log(links)
 articleContent.innerHTML += "links"
