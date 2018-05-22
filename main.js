92c23420d729e11ca688dae43a58a4672f31f701
William Kimball <wkkimball043@gmail.com>
2018-05-22 10:30:47 -0500

    added JS for advanced practice, and CSS to style if JS worked correctly (currently commented out

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 document.querySelector("article").innerHTML += "<h3>Mission statement: I strive to keep horses safe!</h3>";
 
 console.log(document.querySelector("article"))
+
+let info = document.querySelector("#candidate__info");
+let att = document.createAttribute("congressional-district");
+
+att.value = "congressional_district_4";
+
+info.setAttributeNode(att);
